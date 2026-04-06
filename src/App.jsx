3d33ad488fc4adc2b2d57cd5c8704880dfa3b70b
Arjit anand <arjitanand2005@gmail.com>
2026-04-06 23:54:07 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import useLenis from "./hooks/useLenis";
+import UIUXSection from "./components/UIUXSection";
 import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
